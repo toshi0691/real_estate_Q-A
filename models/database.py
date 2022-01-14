d39474a80a2c539a02eb4dfd8bf88464afd98b52
toshi0691 <toshi0905chukyo@gmail.com>
2022-01-14 09:08:12 +0900

    db接続

--- models/database.py
+++ models/database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'onegai.db')
-engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
+databese_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'questions.db')
+#engine = create_engine('sqlite:///' + databese_file, convert_unicode=True)
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/realestate')
 db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
