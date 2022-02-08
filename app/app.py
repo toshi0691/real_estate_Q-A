67660ea319f2df31be1702beef535a9e6be17117
toshi0691 <toshi0905chukyo@gmail.com>
2022-02-08 21:26:38 +0900

    サーバーを立てて画面が見える用に編集

--- app/app.py
+++ app/app.py
@@ -16,6 +16,30 @@ def hello():
 def index():
     return render_template("index.html")
 
+@app.route("/questions")
+def questions():
+    return render_template("questions.html")
+
+@app.route("/answerers")
+def answerers():
+    return render_template("answerers.html")
+
+@app.route("/choose_membership_type")
+def choose_membership_type():
+    return render_template("choose_membership_type.html")
+
+@app.route("/privacypolicy")
+def privacypolicy():
+    return render_template("privacypolicy.html")
+
+@app.route("/register_answerer")
+def register_answerer():
+    return render_template("register_answerer.html")
+
+@app.route("/register_user")
+def register_user():
+    return render_template("register_user.html")
+
 
 #おまじない
 if __name__ == "__main__":
